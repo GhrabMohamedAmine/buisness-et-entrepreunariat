e05f954c714d3f3c771e5fb158c3d230a4d87dc8
tez139 <moetez.benattia02@gmail.com>
2026-02-23 01:42:21 +0100

    fixing some dtatbase errors

--- src/main/java/utils/MyDatabase.java
+++ src/main/java/utils/MyDatabase.java
@@ -3,15 +3,55 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class MyDatabase
-{
-    // MAMP default credentials
-    private static final String URL = "jdbc:mysql://localhost:8889/Nexum?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASS = "root"; // MAMP default is 'root'
+//public class MyDatabase
+//{
+//    // MAMP default credentials
+//    private static final String URL = "jdbc:mysql://localhost:8889/Nexum?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+//    private static final String USER = "root";
+//    private static final String PASS = "root"; // MAMP default is 'root'
+//
+//    public static Connection getConnection() throws SQLException {
+//        return DriverManager.getConnection(URL, USER, PASS);
+//    }
+//
+//}
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASS);
+
+public class MyDatabase {
+
+    private static final String URL ="jdbc:mysql://localhost:8889/Nexum?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USERNAME ="root";
+    private static final String PWD ="root";
+
+    private  static Connection conx;
+
+    public static MyDatabase instance;
+
+    private MyDatabase() throws SQLException {
+        conx = DriverManager.getConnection(URL,USERNAME,PWD);
+        System.out.println("Connexion établie!");
     }
 
+    public static synchronized MyDatabase getInstance(){
+        if (instance == null){
+            try {
+                instance = new MyDatabase();
+            } catch (SQLException e) {
+                throw new RuntimeException("Unable to initialize database connection", e);
+            }
+        }
+        return instance;
+
+    }
+
+
+    public static synchronized Connection getConnection() throws SQLException {
+        if (instance == null) {
+            getInstance();
+        }
+        if (conx == null || conx.isClosed() || !conx.isValid(2)) {
+            conx = DriverManager.getConnection(URL, USERNAME, PWD);
+        }
+        return conx;
+    }
 }
